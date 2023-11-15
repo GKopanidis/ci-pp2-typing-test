3caf6a1f4878495d57c3465ced5d52bca9cab697
Georgios Kopanidis <kopanidis@me.com>
2023-11-15 09:58:59 +0000

    Add random quotes generator

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Generates random quotes 
+ */
+const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
+const shownTextSection = document.getElementById("shownText");
+const userInput = document.getElementById("shownTextInput");
+
+console.log(shownTextSection, userInput);
