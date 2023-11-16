2d538f552046cfc8631a072fbb6fbedb91424180
Georgios Kopanidis <kopanidis@me.com>
2023-11-16 08:49:45 +0000

    Add modal for instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,10 @@ let lastCorrectIndex = -1;
 let correctIndexes = new Set();
 let incorrectIndexes = new Set();
 let isTestCompleted = false;
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
 
 /**
  * Display random quote
@@ -43,7 +47,6 @@ userInput.addEventListener("input", () => {
     let quoteChars = document.querySelectorAll(".quote-chars");
     quoteChars = Array.from(quoteChars);
     let userInputChars = userInput.value.split("");
-    correctWords = 0;
 
     quoteChars.forEach((char, index) => {
         if (char.innerText == userInputChars[index]) {
@@ -54,24 +57,23 @@ userInput.addEventListener("input", () => {
         } else {
             if (char.classList.contains("correctWords")) {
                 char.classList.remove("correctWords");
+                correctIndexes.delete(index);
             }
-            if (index < userInputChars.length && !char.classList.contains("incorrectWords")) {
+            if (index < userInputChars.length) {
                 char.classList.add("incorrectWords");
-                if (!incorrectIndexes.has(index)) {
-                    incorrectWords++;
-                    incorrectIndexes.add(index);
-                }
+                incorrectIndexes.add(index);
             }
         }
     });
 
     correctWords = correctIndexes.size;
+    incorrectWords = incorrectIndexes.size;
 
     document.getElementById("correctWords").innerText = correctWords;
     document.getElementById("incorrectWords").innerText = incorrectWords;
 
-    // Returns true if all chars are entered correctly
-   isTestCompleted = quoteChars.length === correctIndexes.size;
+    isTestCompleted = (quoteChars.length === correctIndexes.size) && (userInput.value.length === quoteChars.length);
+
     if (isTestCompleted) {
         displayResult();
     }
@@ -81,6 +83,9 @@ userInput.addEventListener("input", () => {
  * Start Test
  */
 const startTest = () => {
+    if (timer) {
+        clearInterval(timer);
+    }
     userInput.disabled = false;
     userInput.value = "";
     correctWords = 0;
@@ -89,6 +94,8 @@ const startTest = () => {
     correctIndexes.clear();
     incorrectIndexes.clear()
     timer = "";
+    document.getElementById("correctWords").innerText = correctWords;
+    document.getElementById("incorrectWords").innerText = incorrectWords;
     timeReduce();
     document.getElementById("start-test").style.display = "none";
     document.getElementById("stop-test").style.display = "block";
@@ -119,7 +126,7 @@ const timeReduce = () => {
  * Update timer
  */
 function updateTimer() {
-    if(time == 0) {
+    if (time == 0) {
         //End test if timer reaches 0
         displayResult();
     }
@@ -138,23 +145,55 @@ const displayResult = () => {
     document.getElementById("stop-test").style.display = "none";
     userInput.disabled = true;
     let timeTaken = 1;
-    if(time != 0) {
+    if (time != 0) {
         timeTaken = (60 - time) / 100;
     }
-    document.getElementById("cpm").innerText = (userInput.value.length / 5 / timeTaken).toFixed(2) + " cpm";
-    document.getElementById("accuracy").innerText = Math.round(((userInput.value.length - incorrectWords) / userInput.value.length) * 100) + "%";
+    document.getElementById("cpm").innerText = (userInput.value.length / 5 / timeTaken).toFixed(0) + " cpm";
+
+    let accuracy = Math.round(((userInput.value.length - incorrectWords) / userInput.value.length) * 100);
+    document.getElementById("accuracy").innerText = accuracy + "%";
+    // Check, if accuracy is 100%
+    if (accuracy === 100) {
+        // Show text, if accuracy is 100%
+        document.getElementById("godlike").style.display = "block";
+        document.getElementById("godlike").innerText = "GODLIKE :D";
+    } else {
+        // Hide text, if accuracy isnt 100%
+        document.getElementById("godlike").style.display = "none";
+    }
+
     document.getElementById("keystrokes").innerText = + keystrokes;
     document.getElementById("start-test").style.display = "block";
     isTestCompleted = false;
 };
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
     let resultDisplayed = document.querySelector(".result").style.display === "block";
-
-    if (event.key === "Enter" && (resultDisplayed || testCompleted)) {
-        userInput.disabled = false;
-        setTimeout(() => {
+    if (event.key === "Enter") {
+        if (isTestCompleted && !resultDisplayed && userInput.value.length > 0) {
+            displayResult();
+        } else if (resultDisplayed) {
             startTest();
-        }, 0);
+        }
     }
 });
+
+/**
+ * Modal box
+ */
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
